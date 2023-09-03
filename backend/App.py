59b9371be06d19447f12233d2cb7bed4038d0ada
Zodiako <cristianjvz98@gmail.com>
2023-09-03 11:15:17 -0500

    feat: configurado cors para comunucacion con el front

--- backend/App.py
+++ backend/App.py
@@ -1,11 +1,12 @@
 from routes.evaluador.evaluador import evaluador_blueprint
 from flask import Flask
+from flask_cors import CORS
 from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Leahtovar2017.@localhost/I.R.A'
 app.secret_key = 'mysecretkey'
-
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 db.init_app(app)
 
 # Importar el objeto Blueprint y registrar la ruta en la aplicación
