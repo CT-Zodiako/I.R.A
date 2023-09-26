14f68ea2033b1acaecc9978caf5236eec486c978
Pancho Polanco <cristian.pancho.cami@gmail.com>
2023-09-26 16:36:28 -0500

    feature: lista de estudiantes tabla

--- frontend/src/components/Evaluadores/ListaEstudiantes.jsx
+++ frontend/src/components/Evaluadores/ListaEstudiantes.jsx
@@ -1,8 +1,23 @@
 import { useEffect, useState } from "react";
+import evaluadorService from '../../services/servicioEvaluador';
 
 export const VistaEstudiantes = () => {
     
-    
+    const[listaEstudiantes, setListaEstudiantes] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+          try {
+            const data = await evaluadorService.estudiantesEvaluador();
+            console.log(data)
+            setListaEstudiantes(data);
+          } catch (error) {
+            console.error('Error al obtener la lista de examenes:', error);
+          }
+        }
+        fetchData();
+      }, []);
+
     return(
         <>
             <div>
@@ -10,21 +25,20 @@ export const VistaEstudiantes = () => {
                     <table>
                         <thead>
                         <tr>
-                            <th>Programa</th>
-                            <th>Titulo</th>
-                            <th>Estado</th>
-                            <th>Acciones</th>
+                            <th>Nombre Estudiante</th>
+                            <th>Correo</th>
+                            <th>Telefono</th>
+                            <th>Codigo</th>
                         </tr>
                         </thead>
                         <tbody>
-                        {listaExamenes.map((examenes) => (
-                        <tr key={examenes.id}>
-                            <td>{examenes.programa}</td>
-                            <td>{examenes.proyecto_integrador}</td>
+                        {listaEstudiantes.map((data) => (
+                        <tr key={data.id}>
+                            <td>{data.estudiantes}</td>
+                            <td></td>
                             <td>Pendiente</td>
                             <td>
                                 <div>
-                                    <button onClick={(e) => onEditarEvaluador(evaluador.id)}><Link to="/gestion-usuario">Editar</Link></button>
                                     {/* <button onClick={(e) => onCambiarEstadoEvaluador(e, evaluador.id)}>{evaluador.estado ? 'Desactivar' : 'Activar'}</button> */}
                                     {/* <button onClick={(e) => onEditarEvaluador(evaluador.id)}><Link to="/gestion-usuario">Editar</Link></button> */}
                                     {/* <button onClick={(e) => onEliminarEvaluador(e, evaluador.id)}>Eliminar</button> */}
