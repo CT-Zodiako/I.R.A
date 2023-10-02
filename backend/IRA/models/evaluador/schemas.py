dbf01ea7bba3ede1d44bb88bb73747d59af329a3
Zodiako <cristianjvz98@gmail.com>
2023-10-02 12:00:40 -0500

    fix: nombre de variable en el eesquema de examen por evaluador

--- backend/IRA/models/evaluador/schemas.py
+++ backend/IRA/models/evaluador/schemas.py
@@ -6,17 +6,21 @@ from ...models.examen.examen_model import Examen
 class EvaluadorSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Evaluador
-        fields = ("id", "nombre_evaluador", "correo", "numero_identificacion","telefono","estado")
+        fields = ("id", "nombre_evaluador", "correo",
+                  "numero_identificacion", "telefono", "estado")
         load_instance = True
+
 
 class EvaluadorConrasenaSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Evaluador
-        fields = ("numero_identificacion","contrasenna","nombre_evaluador")
+        fields = ("numero_identificacion", "contrasenna", "nombre_evaluador")
         load_instance = True
+
 
 class ExamenEvaluadorSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Examen
-        fields = ("id", "programa", "proyecto_integrador", "actividades_formativas", "estudiantes")
+        fields = ("id", "programa_id", "proyecto_integrador",
+                  "actividades_formativas", "estudiantes")
         load_instance = True
