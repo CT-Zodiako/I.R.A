edb3b0ae75d580249182bb272a30a0aef644205b
Zodiako <cristianjvz98@gmail.com>
2023-10-02 11:10:55 -0500

    feat: tabla de programas y creacion de proyectos basado en id del programa

--- backend/IRA/models/examen/examen_model.py
+++ backend/IRA/models/examen/examen_model.py
@@ -1,10 +1,12 @@
 from ...db import db
 from ...models.relaciones.relacion_examen_evaluador import examen_evaluador_tabla
+from ...models.programa.programas_model import Programa
 
 
 class Examen(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    programa = db.Column(db.String(255), nullable=False)
+    programa_id = db.Column(db.Integer, db.ForeignKey(Programa.id), nullable=False)
+    # programa_id = db.Column(db.Integer, db.ForeignKey('programa.id'), nullable=False)
     proyecto_integrador = db.Column(db.String(255), nullable=False)
     actividades_formativas = db.Column(db.JSON)
     estudiantes = db.Column(db.JSON)
@@ -13,8 +15,8 @@ class Examen(db.Model):
     evaluadores_relacion = db.relationship(
         'Evaluador', secondary=examen_evaluador_tabla, back_populates='examenes_evaluador_relacion')
 
-    def __init__(self, programa, proyecto_integrador, actividades_formativas, estudiantes, resultado_aprendizaje_id):
-        self.programa = programa
+    def __init__(self, programa_id, proyecto_integrador, actividades_formativas, estudiantes, resultado_aprendizaje_id):
+        self.programa_id = programa_id
         self.proyecto_integrador = proyecto_integrador
         self.actividades_formativas = actividades_formativas
         self.estudiantes = estudiantes
