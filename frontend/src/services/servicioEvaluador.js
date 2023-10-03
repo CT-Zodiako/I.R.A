a400a1def663bf276b98c7643a06072f002671d0
Pancho Polanco <cristian.pancho.cami@gmail.com>
2023-10-03 14:50:51 -0500

    feature: traer las actividades del estudiante por calificar

--- frontend/src/services/servicioEvaluador.js
+++ frontend/src/services/servicioEvaluador.js
@@ -69,11 +69,12 @@ class evaluadorService{
     }
   }
 
-  async calificacionEvaluador(id) {
+  async calificacionEvaluador(id_examen) {
     try{
-      const responce = await axios.get(`http://127.0.0.1:3001/evaluador/examenes_evaluador/1`);
-      return(responce.data.data.actividades_formativas)
-
+      
+      const responce = await axios.get(`http://127.0.0.1:3001/calificacion/actividades_examen`, id_examen);
+      console.log(responce.data)
+      return(responce.data)
     }catch(err){
       console.error(err)
     }
