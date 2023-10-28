60eb78a28cb394e80ba034a003ea988f60de7ccf
Zodiako <cristianjvz98@gmail.com>
2023-10-28 16:03:43 -0500

    fix:

--- backend/IRA/routes/informes/informes_route.py
+++ backend/IRA/routes/informes/informes_route.py
@@ -13,6 +13,8 @@ def traer_calificaciones():
 from flask import jsonify, request, make_response
 
 
+from flask import jsonify
+
 @informes_blueprint.route('/calificacion_by_id_examen/<int:examen_id>', methods=['GET'])
 def obtener_calificaciones_por_examen(examen_id):
     # Realiza una consulta para obtener la calificación relacionada con el examen_id
@@ -24,24 +26,19 @@ def obtener_calificaciones_por_examen(examen_id):
     # Obtén el campo "calificacion" del objeto CalificacionExamen
     calificacion_lista = calificacion_examen.calificacion
 
-    # Inicializa variables para calcular el promedio
-    suma_notas = 0
-    cantidad_notas = 0
-
+    # Recorre cada elemento de la lista de calificaciones
     for elemento in calificacion_lista:
-        # Si hay un elemento con clave "notas", sumar sus valores
-        if "notas" in elemento.get("calificacion", {}):
-            suma_notas += sum(elemento["calificacion"]["notas"])
-            cantidad_notas += len(elemento["calificacion"]["notas"])
+        # Si el elemento tiene la clave "calificacion" y esta contiene "notas"
+        if "calificacion" in elemento and "notas" in elemento["calificacion"]:
+            # Calcula el promedio de notas
+            notas = elemento["calificacion"]["notas"]
+            promedio = sum(notas) / len(notas) if len(notas) > 0 else None
+            # Agrega el promedio al elemento
+            elemento["calificacion"]["promedio"] = promedio
 
-    # Calcula el promedio
-    promedio = suma_notas / cantidad_notas if cantidad_notas > 0 else None
-
-    # Agrega el promedio al JSON de respuesta
-    calificacion_lista.append({"promedio": promedio})
-
-    
+    # Devuelve la lista de calificaciones actualizada con los promedios
     return jsonify(calificaciones=calificacion_lista)
+
 
 
 
