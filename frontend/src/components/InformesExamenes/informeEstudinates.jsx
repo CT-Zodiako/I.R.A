021bccfad96dd0aab37ecd56216d65aa77536bc0
Pancho Polanco <cristian.pancho.cami@gmail.com>
2023-10-30 19:15:49 -0500

    feature: se agrego la opcion de poder descargar el informe en formato PDF

--- frontend/src/components/InformesExamenes/informeEstudinates.jsx
+++ frontend/src/components/InformesExamenes/informeEstudinates.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import Chart from "react-google-charts";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import informeServicio from '../../services/ServicioInforme'
 import evaluadorService from '../../services/servicioEvaluador'
 
@@ -9,6 +11,7 @@ export const PromedioEstudiante =  () => {
     const [calificaciones, setCalificaciones] = useState([]);
     const [conteo, setConteo] = useState({});
     const [notasCalificacion, setNotasCalificacion] = useState([]);
+    const tableRef = useRef(null);
 
     const coloresHexadecimales = notasCalificacion.map(item => item.color);
 
@@ -25,12 +28,24 @@ export const PromedioEstudiante =  () => {
         Object.entries(conteo).map(([key, value]) => [key, value])
     );
 
+    const downloadPDF = () => {
+        const input = document.getElementById("pdf-content");
+        html2canvas(input).then((canvas) => {
+            const imgData = canvas.toDataURL("image/png");
+            const pdf = new jsPDF();
+            const imgProps = pdf.getImageProperties(imgData);
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+            pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+            pdf.save("download.pdf");
+        });
+    };
+
     useEffect(() => {
         async function fetchData() {
           try {
             const data = await informeServicio.promedioEstudiante(evaluadorId);
             setCalificaciones(data);
-            console.log(data);
           } catch (error) {
             console.error('Error al obtener el promedio de los estudiantes:', error);
           }
@@ -43,7 +58,6 @@ export const PromedioEstudiante =  () => {
           try {
             const data = await informeServicio.conteoEstudiante(evaluadorId);
             setConteo(data);
-            console.log(data);
           } catch (error) {
             console.error('Error al obtener el conteo:', error);
           }
@@ -56,7 +70,6 @@ export const PromedioEstudiante =  () => {
           try {
             const data = await evaluadorService.calificacionEstudiante();
             setNotasCalificacion(data);
-            console.log(data);
           } catch (error) {
             console.error('Error al obtener los datos de calificaciones', error);
           }
@@ -66,40 +79,43 @@ export const PromedioEstudiante =  () => {
     
     return(
         <>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Estudiante</th>
-                        <th>Promedio</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {calificaciones.map((promedio, Index) => (
-                        <tr key={Index}>
-                            <td style={{ backgroundColor: onColorPromedio(promedio.calificacion.promedio) }}>
-                                {promedio.nombre}
-                            </td>
-                            <td style={{ backgroundColor: onColorPromedio(promedio.calificacion.promedio) }}>
-                                {promedio.calificacion.promedio}
-                            </td>
+            <div id="pdf-content">
+                <table ref={tableRef}>
+                    <thead>
+                        <tr>
+                            <th>Estudiante</th>
+                            <th>Promedio</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <div>
-                <Chart
-                    width={'600px'}
-                    height={'300px'}
-                    chartType="PieChart"
-                    loader={<div>Cargando gráfico</div>}
-                    data={datosGrafico}
-                    options={{
-                        title: 'Conteo Por Calificacion',
-                        colors: coloresHexadecimales,                     
-                    }}
-                    rootProps={{ 'data-testid': '1' }}
-                />
+                    </thead>
+                    <tbody>
+                        {calificaciones.map((promedio, Index) => (
+                            <tr key={Index}>
+                                <td style={{ backgroundColor: onColorPromedio(promedio.calificacion.promedio) }}>
+                                    {promedio.nombre}
+                                </td>
+                                <td style={{ backgroundColor: onColorPromedio(promedio.calificacion.promedio) }}>
+                                    {promedio.calificacion.promedio}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div>
+                    <Chart
+                        width={'600px'}
+                        height={'300px'}
+                        chartType="PieChart"
+                        loader={<div>Cargando gráfico</div>}
+                        data={datosGrafico}
+                        options={{
+                            title: 'Conteo Por Calificacion',
+                            colors: coloresHexadecimales,                     
+                        }}
+                        rootProps={{ 'data-testid': '1' }}
+                    />
+                </div>
             </div>
+            <button onClick={downloadPDF}>Descargar PDF</button>
         </>
     );
-}
+};
