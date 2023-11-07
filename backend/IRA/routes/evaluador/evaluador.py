7d4e3dddf0e1fb74cdb3148df997685eed59d1e3
Zodiako <cristianjvz98@gmail.com>
2023-11-07 15:26:24 -0500

    feat: creado proteccion de ruta en jwt segun rol

--- backend/IRA/routes/evaluador/evaluador.py
+++ backend/IRA/routes/evaluador/evaluador.py
@@ -1,20 +1,44 @@
 from flask import Blueprint, request, jsonify
 from ...controller.evaluador.evaluador_controller import agregar_evaluador, traer_evaluadores_db, traer_evaluadores_examen_db, eliminar_evaluador_sf, traer_evaluador_por_id,actualizar_evaluador_db,traer_estudiantes_examen_db
-from ...db import db
-from ...models.evaluador.evaluador_model import Evaluador
 from flask_jwt_extended import jwt_required
+from functools import wraps
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+
+
+def admin_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()  # Verificar que el token JWT esté presente
+        current_user = get_jwt_identity()  # Obtener los datos del usuario del token
+        if current_user['rol'] == 'Admin':
+            return fn(*args, **kwargs)
+        else:
+            return jsonify({"message": "Acceso no autorizado"}), 403  # 403 significa acceso prohibido
+    return wrapper
+
+def evaluador_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()  # Verificar que el token JWT esté presente
+        current_user = get_jwt_identity()  # Obtener los datos del usuario del token
+        if current_user['rol'] == 'Evaluador':
+            return fn(*args, **kwargs)
+        else:
+            return jsonify({"message": "Acceso no autorizado"}), 403  # 403 significa acceso prohibido
+    return wrapper  
 
 evaluador_blueprint = Blueprint('evaluador', __name__)
 
 
 @evaluador_blueprint.route('/agregar_evaluador', methods=['POST'])
-@jwt_required()
+@admin_required
 def crear_evaluador():
     data = request.json
     return agregar_evaluador(data)
 
 
 @evaluador_blueprint.route('/traer_evaluadores', methods=['GET'])
+@evaluador_required
 def traer_evaluadores():
     return traer_evaluadores_db()
 
