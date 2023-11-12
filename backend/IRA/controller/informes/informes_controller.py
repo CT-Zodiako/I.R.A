5d05052a959f11a9b326e9cfcddcac8934ace93f
Zodiako <cristianjvz98@gmail.com>
2023-11-12 16:17:39 -0500

    feat: proemdio de estudiante por actividad

--- backend/IRA/controller/informes/informes_controller.py
+++ backend/IRA/controller/informes/informes_controller.py
@@ -15,17 +15,6 @@ class CalificacionEnum(Enum):
     NO_CUMPLE = {'label': 'NO CUMPLE', 'color': 'gray', 'nota': 1}
     NINGUNA_CALIFICACION = {'label': 'NINGUNA CALIFICACION', 'color': 'yellow', 'nota': 0}
 
-# ... (Importaciones y definiciones de Enum)
-
-# ... (Importaciones y definiciones de Enum)
-
-def clasificar_calificacion(promedio):
-    for nota in CalificacionEnum:
-        if nota.value['nota'] == int(promedio):
-            return nota.value['label']
-
-# ... (Importaciones y definiciones de Enum)
-
 def clasificar_calificacion(promedio):
     for nota in CalificacionEnum:
         if nota.value['nota'] == int(promedio):
@@ -39,6 +28,7 @@ def traer_calificaciones_por_examen(examen_id):
 
     calificaciones_serializables = []
     promedios_estudiantes = defaultdict(list)
+    promedios_actividades = defaultdict(lambda: defaultdict(list))
     conteo_calificaciones = defaultdict(int)
 
     for calificacion_examen in calificaciones_examenes:
@@ -52,35 +42,45 @@ def traer_calificaciones_por_examen(examen_id):
         for estudiante in calificacion_examen.calificacion:
             nombre_estudiante = estudiante["nombre"]
             notas_estudiante = estudiante["calificacion"]["notas"]
-            promedio_notas = round(sum(notas_estudiante) / len(notas_estudiante)) if len(notas_estudiante) > 0 else None
+            promedio_estudiante = round(sum(notas_estudiante) / len(notas_estudiante)) if len(notas_estudiante) > 0 else None
 
             calificacion_estudiante = {
                 "nombre": nombre_estudiante,
                 "calificacion": {
                     "notas": notas_estudiante,
                     "observaciones": estudiante["calificacion"]["observaciones"],
-                    "promedio": promedio_notas
+                    "promedio": promedio_estudiante
                 }
             }
 
             calificacion_serializable["calificacion"].append(calificacion_estudiante)
 
             # Almacenar los promedios por estudiante
-            promedios_estudiantes[nombre_estudiante].append(promedio_notas)
+            promedios_estudiantes[nombre_estudiante].append(promedio_estudiante)
+
+            # Almacenar las notas por actividad para el promedio por actividad
+            for i, nota in enumerate(notas_estudiante):
+                actividad = f"Actividad{i + 1}"
+                promedios_actividades[actividad][nombre_estudiante].append(nota)
 
         calificaciones_serializables.append(calificacion_serializable)
 
+    # Calcular el promedio de cada actividad por estudiante
+    promedio_actividades_estudiantes = defaultdict(lambda: defaultdict(float))
 
+    for actividad, promedios_por_estudiante in promedios_actividades.items():
+        for estudiante, notas_actividad in promedios_por_estudiante.items():
+            promedio_actividades_estudiantes[actividad][estudiante] = round(sum(notas_actividad) / len(notas_actividad)) if len(notas_actividad) > 0 else None
 
     for estudiante, promedios in promedios_estudiantes.items():
         promedio_final = round(sum(promedios) / len(promedios)) if len(promedios) > 0 else None
         calificacion_final = clasificar_calificacion(promedio_final)
-
 
         # Agregar al conteo
         conteo_calificaciones[calificacion_final] += 1
 
     return jsonify(
         calificaciones=calificaciones_serializables,
+        promedio_actividades_estudiantes=promedio_actividades_estudiantes,
         conteo=conteo_calificaciones
     )
