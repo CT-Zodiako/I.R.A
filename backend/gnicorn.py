2d048d1bee926bef7c30ee07764f56d3661f5e46
Zodiako <cristianjvz98@gmail.com>
2024-02-19 18:29:30 -0500

    crear admin

--- backend/gnicorn.py
+++
@@ -1,7 +0,0 @@
-# gunicorn_config.py
-
-import multiprocessing
-
-bind = "0.0.0.0:8000"  # Dirección y puerto en los que Gunicorn debe escuchar
-# Número de procesos de trabajadores para Gunicorn
-workers = multiprocessing.cpu_count() * 2 + 1
