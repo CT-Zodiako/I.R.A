e459d380ed2662eae6990e8d939b2ef7d56c48a6
Pancho Polanco <cristian.pancho.cami@gmail.com>
2024-02-19 12:20:17 -0500

    feature: add img

--- frontend/src/view/InicioSesion.jsx
+++ frontend/src/view/InicioSesion.jsx
@@ -4,8 +4,7 @@ import { useDispatch } from "react-redux";
 import { iniciarSesion } from "../redux/inicioSesionSlipe";
 import axios from "axios";
 import { Button, TextField } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import unimayor from "../img/unimayor.png";
+import unimayor from "../../public/img/unimayor.png";
 import PersonIcon from '@mui/icons-material/Person';
 import LockIcon from '@mui/icons-material/Lock';
 
