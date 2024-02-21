627639e167f7e81c8b53568d73d20654093e7309
Zodiako <cristianjvz98@gmail.com>
2024-02-21 12:54:43 -0500

    feat: :sparkles: ruta de resultados respecto a estado

--- backend/IRA/routes/resultado_aprendizaje/resultado_aprendizaje_route.py
+++ backend/IRA/routes/resultado_aprendizaje/resultado_aprendizaje_route.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from ...controller.resultados_aprendizaje.resultados_aprendizaje_controller import crear_resultado_aprendizaje,  traer_resultados_aprendizaje, cambiar_estado_resultado_db
+from ...controller.resultados_aprendizaje.resultados_aprendizaje_controller import crear_resultado_aprendizaje,  traer_resultados_aprendizaje, cambiar_estado_resultado_db,get__resultados_by_estado
 from ...auth import admin_required
 
 resultado_aprendizaje_blueprint = Blueprint('resultado_aprendizaje', __name__)
@@ -8,15 +8,17 @@ resultado_aprendizaje_blueprint = Blueprint('resultado_aprendizaje', __name__)
 @resultado_aprendizaje_blueprint.route('/crear_resultado', methods=['POST'])
 @admin_required
 def crear_resultado():
-    data= request.get_json()
+    data = request.get_json()
     resultado = crear_resultado_aprendizaje(data)
     return resultado
+
 
 @resultado_aprendizaje_blueprint.route('/traer_resultados', methods=['GET'])
 @admin_required
 def traer_resultados():
     resultados = traer_resultados_aprendizaje()
-    return  jsonify(resultados)
+    return jsonify(resultados)
+
 
 @resultado_aprendizaje_blueprint.route('/cambiar_estado_resultado/<int:resultado_id>', methods=['PUT'])
 @admin_required
@@ -24,10 +26,8 @@ def cambiar_estado_resultado(resultado_id):
     return cambiar_estado_resultado_db(resultado_id)
 
 
-
-
-
-
-
-    
-
+@resultado_aprendizaje_blueprint.route('/traerResultadoByEstado', methods=['GET'])
+@admin_required
+def traer_resultado_by_estado():
+    resultados = get__resultados_by_estado()
+    return jsonify(resultados)
