406351be0bcfcc25eb61f2f9a50ae56569ea39c9
Pancho Polanco <cristian.pancho.cami@gmail.com>
2024-03-17 17:35:03 -0500

    add las graficas por actividad y la general con su respectivo color

--- frontend/src/components/InformesExamenes/ModalInformeExamen.jsx
+++ frontend/src/components/InformesExamenes/ModalInformeExamen.jsx
@@ -1,8 +1,5 @@
 import { useEffect, useState, useRef } from "react";
 import Chart from "react-google-charts";
-// import jsPDF from "jspdf";
-// import html2canvas from "html2canvas";
-// import html2pdf from "html2pdf.js";
 import informeServicio from "../../services/ServicioInforme";
 import evaluadorService from "../../services/servicioEvaluador";
 import {
@@ -19,14 +16,32 @@ export const ModalInformeExamen = ({ abrirInforme, cerrarInforme, descargarPDF }
   const evaluador = useSelector((state) => state.informeExamen.idInforme);
   
   const [informacionCalificaciones, setInformacionCalificaciones] = useState([]);
-  console.log('Estado de calificaciones: ', informacionCalificaciones);
+  // console.log('Estado de calificaciones: ', informacionCalificaciones);
   const [promedioGeneral, setPromedioGeneral] = useState([]);
-  console.log('Estado de promedioGrafica: ', promedioGeneral);
+  // console.log('Estado de promedioGrafica: ', promedioGeneral);
   const [actividadesDecripcion, setActividadesDescripcion] = useState([]);
-  console.log('Estado de actividades: ', actividadesDecripcion);
+  // console.log('Estado de actividades: ', actividadesDecripcion);
   const [colorInforme, setColorInforme] = useState([]);  
   console.log('Estado de colorInforme: ', colorInforme);
-  
+
+
+  const [graficaGeneral, setGraficaGeneral] = useState([]);
+  const [graficaActividades, setGraficaActividades] = useState([]);  
+
+  const graficoGeneral = [["Actividad", "Calificaciones"]].concat(
+    Object.entries(graficaGeneral).map(([key, value]) => [key, value])
+  );  
+
+  const coloresFondoPastel = (categorias) => {
+    const colorFondo =  categorias.map(categoria => {      
+      const color = colorInforme.find((colorNota) => colorNota.label.toLowerCase() === categoria);
+      if (color) {
+        return { color: color.color };
+      } 
+    });
+
+    return colorFondo;
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -52,7 +67,6 @@ export const ModalInformeExamen = ({ abrirInforme, cerrarInforme, descargarPDF }
           const data = await informeServicio.promedioGraficaGeneral(evaluador);
           setPromedioGeneral(data);
         }
-        console.log("graficas actividad: ", data);
       } catch (error) {
         console.error("Error al obtener el conteo:", error);
       }
@@ -88,6 +102,35 @@ export const ModalInformeExamen = ({ abrirInforme, cerrarInforme, descargarPDF }
     fetchData();
   }, []);
 
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        if(evaluador){
+          const data = await informeServicio.graficoInformeGeneral(evaluador);
+          setGraficaGeneral(data);
+        }
+      } catch (error) {
+        console.error("Error al obtener el conteo:", error);
+      }
+    }
+    fetchData();
+  }, [evaluador]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        if(evaluador){
+          const data = await informeServicio.graficoInformeActividades(evaluador);
+          setGraficaActividades(data);
+        }
+      } catch (error) {
+        console.error("Error al obtener el conteo:", error);
+      }
+    }
+    fetchData();
+  }, [evaluador]);
+
   return (
     <>
       <Dialog
@@ -103,7 +146,49 @@ export const ModalInformeExamen = ({ abrirInforme, cerrarInforme, descargarPDF }
         </DialogTitle>
         <DialogContent>
           <DialogContentText>
-            
+            <h1>Grafico General:</h1>
+              <Chart
+                width={"600px"}
+                height={"400px"}
+                chartType="PieChart"
+                loader={<div>Cargando Grafico</div>}
+                data={[["Actividad", "Calificaciones"]].concat(
+                  Object.entries(graficaGeneral).map(([calificacion, value]) => [calificacion, value])
+                )}
+                options={{
+                  chart: {
+                    title: "Promedio de calificaciones",
+                  },
+                  slices: coloresFondoPastel(Object.keys(graficaGeneral)),
+                }}
+                rootProps={{ "data-testid": "1" }}
+              />
+
+            <h1>Grafico Por Actividades:</h1>
+            <div>
+              {Object.entries(graficaActividades).map(
+                ([actividad, categorias], index) => (
+                <div key={actividad}>
+                  <Chart
+                    sx={{ width: "100%", height: "100%" }}
+                    chartType="PieChart"
+                    loader={<div>Cargando gráfico</div>}
+                    data={[
+                      ["Actividades", "Cantidades"],
+                      ...Object.entries(categorias).map(([categoria, cantidad]) => [categoria, cantidad])
+                    ]}
+                    options={{
+                      title: `Gráfica Actividad ${actividad}`,
+                      titleTextStyle: {
+                        fontSize: 22,
+                      },
+                      slices: coloresFondoPastel(Object.keys(categorias)),
+                    }}
+                    rootProps={{ "data-testid": "1" }}
+                  />
+                </div>
+              ))}
+            </div>
           </DialogContentText>
         </DialogContent>
         <DialogActions sx={{ background: "rgba( 0, 0, 0 , 0.2)" }}>
--- frontend/src/services/ServicioInforme.js
+++ frontend/src/services/ServicioInforme.js
@@ -1,3 +1,4 @@
+import { convertLength } from '@mui/material/styles/cssUtils';
 import axios from 'axios';
 
 class informeServicio{
@@ -26,16 +27,34 @@ class informeServicio{
     } catch (err) {
       console.error(err);
     }
- }
-
- async actividadesDescripcion(evaluadorId) {
-  try {
-    const response = await axios.get(`${import.meta.env.VITE_API_URL}/examen/actividades/${evaluadorId}`);
-      return response.data;
-  } catch (err) {
-    console.error(err);
   }
-}
+
+  async actividadesDescripcion(evaluadorId) {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/examen/actividades/${evaluadorId}`);
+        return response.data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async graficoInformeGeneral(evaluadorId) {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/informes/informe/${evaluadorId}`);
+      return response.data;      
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async graficoInformeActividades(evaluadorId) {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/informes/informeActividad/${evaluadorId}`);
+      return response.data;      
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
 export default new informeServicio();
